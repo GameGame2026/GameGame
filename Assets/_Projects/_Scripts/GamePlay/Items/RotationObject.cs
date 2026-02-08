e37ec1c83c4e0a7d21c291b7660d844f64d789c8
Concorde0 <1462913972@qq.com>
2026-02-09 05:44:15 +0800

    feat(rotation): 完成物体旋转

---
+++ Assets/_Projects/_Scripts/GamePlay/Items/RotationObject.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using _Projects.GamePlay;
+using UnityEngine;
+
+public class RotationObject : DisposableObject
+{
+    public float hoverSpeed = 1f;
+    public float hoverHeight = 0.5f;
+    public float rotateSpeed = 30f;
+    
+    public bool stop = false;
+
+    private Vector3 startPos;
+    
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    public override void ChangeState()
+    {
+        base.ChangeState();
+        if (IsAttached)
+        {
+            stop = true;
+            Rotation();
+        }
+    }
+
+    public override void Recycle()
+    {
+        base.Recycle();
+        Rotation();
+        stop = false;
+
+    }
+
+    private void Update()
+    {
+        if (!stop) { Rotation(); }
+        
+    }
+
+    private void Rotation()
+    {
+        // 上下悬浮
+        float newY = startPos.y + Mathf.Sin(Time.time * hoverSpeed) * hoverHeight;
+        transform.position = new Vector3(startPos.x, newY, startPos.z);
+
+        // 匀速自转
+        transform.Rotate(Vector3.right, rotateSpeed * Time.deltaTime);
+        
+    }
+
+   
+}
