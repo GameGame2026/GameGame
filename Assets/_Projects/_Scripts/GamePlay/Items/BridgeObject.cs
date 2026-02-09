69e22e5541d80b6f075f0b61b72f7aab9da7b7d0
Concorde0 <1462913972@qq.com>
2026-02-10 05:12:13 +0800

    feat(Gameplay): 完成bridgeObject

--- Assets/_Projects/_Scripts/GamePlay/Items/BridgeObject.cs
+++ Assets/_Projects/_Scripts/GamePlay/Items/BridgeObject.cs
@@ -1,7 +1,32 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
 namespace _Projects.GamePlay
 {
     public class BridgeObject : DisposableObject
     {
+        public GameObject bridgePrefab; 
+        public GameObject afterBridgePrefab;
+
+        public bool isOpened = false;
+    
+        public override void ChangeState()
+        {
+            if (!isOpened)
+            {
+                bridgePrefab.SetActive(false);
+                afterBridgePrefab.SetActive(true);
+                isOpened = true;
+            }
+            base.ChangeState();
         
+        }
+
+        public override void Recycle()
+        {
+            bridgePrefab.SetActive(true);
+            afterBridgePrefab.SetActive(false);
+            base.Recycle();
+        }
     }
 }
